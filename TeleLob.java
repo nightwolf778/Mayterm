cb41d6baf1d242a26777ce77ee472ee04344eeaa
nightwolf778 <james4school@gmail.com>
2017-05-05 01:18:08 -0400

    Create TeleLob.java

---
+++ TeleLob.java
@@ -0,0 +1,32 @@
+//TeleLob.java
+package TeleLob;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class TeleLob extends StateBasedGame{
+
+	
+	public TeleLob(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new TeleLob("TeleLob"));
+		app.setDisplayMode(1200, 750, false);
+		app.setAlwaysRender(true);
+		app.start();
+
+	}
+
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException {
+		this.addState(new GameStateMenu());
+		
+	}
+
+}
