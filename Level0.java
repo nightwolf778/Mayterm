c3cd05daa8df802a6e5c3326a411c265bb55b456
WyattBlair <wblair2017@mka.org>
2017-05-12 18:14:57 -0400

    Update Level0.java

--- Level0.java
+++ Level0.java
@@ -11,6 +11,7 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
 import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -38,12 +39,29 @@ public class Level0 extends BasicGameState{
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
 		walls = new ArrayList<Rectangle>();
+		
+		
+		Rectangle wall1 = new Rectangle(0, 0, 1200, 50);
+		Rectangle wall2 = new Rectangle(0, 0, 50, 750);
+		Rectangle wall3 = new Rectangle(0, 700, 1200, 50);
+		Rectangle wall4 = new Rectangle(1150, 0, 50, 750);
+		Rectangle wall5 = new Rectangle(300, 0, 50, 400);
+		Rectangle wall6 = new Rectangle(600, 300, 50, 300);
+		
+		walls.add(wall1);
+		walls.add(wall2);
+		walls.add(wall3);
+		walls.add(wall4);
+		walls.add(wall5);
+		walls.add(wall6);
+		
+	
 		bobFlight = new SpriteSheet("data/TeleLob-flight.png",32,32);
 		bobGround = new SpriteSheet("data/TeleLob-Blink-clone.png",32,32);
 		bobGroundAni = new Animation(bobGround, 100);
 		xBob = 700;
 		yBob = 400;
-		radius = 25;
+		radius = 30;
 		time = 0;
 		velocity = new Vector2f(0, 0);
 		acceleration = 10;
@@ -67,24 +85,39 @@ public class Level0 extends BasicGameState{
 			g.setColor(Color.green);
 			g.fill(r);
 		}
+		g.drawString("xBob: "+xBob, 100, 300);
+		g.drawString("xBobBall: "+bobBall.getX(), 100, 325);
+		g.drawString("yBob: "+yBob, 100, 350);
+		g.drawString("yBobBall: "+bobBall.getY(), 100, 375);
+		g.drawString("Velocity Mag: "+velocity.length(), 100, 400);
+		g.drawString("hitbox Y: "+bobBall.getBox().getMinY(), 100, 425);
+		g.drawString("hitbox X: "+bobBall.getBox().getMaxX(), 100, 450);
+		g.draw(bobBall.getBox());
+		
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame sbg, int delta) throws SlickException {
-		this.bobBall = new Ball(10, velocity, acceleration, xBob, yBob, vyInit);
-		xBob = bobBall.getX(time);
 		
-		if(flying){
-			bobBall.updateVy(time);
-			System.out.println("Got here 0");
-		}
-		this.velocity.set(bobBall.getVelocityX(), bobBall.getVelocityY());
-		yBob = bobBall.getY(time);
-//		bob.setLocation(bobBall.getX(time)-10, bobBall.getY(time)-10);
+		this.bobBall= new Ball(radius, velocity, acceleration, xBob, yBob, vyInit);
 		
 		if (velocity.length() != 0){
-			time += delta/12000f;
+			this.bobBall = new Ball(radius, velocity, acceleration, xBob, yBob, vyInit);
+			xBob = bobBall.getX(time);
+			
+			if(flying){
+				bobBall.updateVy(time);
+//				System.out.println("Got here -1");
+			
+				this.velocity.set(velocity.getX(), bobBall.getVelocityY());
+				yBob = bobBall.getY(time);
+	//			bob.setLocation(bobBall.getX(time)-10, bobBall.getY(time)-10);
+			
+				time += delta/12000f;
+		
+			}
 		}
+			
 		
 		if (container.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON)){
 
@@ -101,67 +134,114 @@ public class Level0 extends BasicGameState{
 			this.vyInit = velocity.getY();
 		} 
 		
-		int width = 800;
-		int height = 600;
-		
-		Rectangle wall1 = new Rectangle(0, 0, width, 50);
-		Rectangle wall2 = new Rectangle(0, 0, 50, height);
-		Rectangle wall3 = new Rectangle(0, height-50, width, 50);
-		Rectangle wall4 = new Rectangle(width-50, 0, 50, height);
-		Rectangle wall5 = new Rectangle(300, 0, 50, 400);
-		Rectangle wall6 = new Rectangle(600, 300, 50, 300);
-		
-		walls.add(wall1);
-		walls.add(wall2);
-		walls.add(wall3);
-		walls.add(wall4);
-		walls.add(wall5);
-		walls.add(wall6);
-		
-		for (int i = 0; i < walls.size()-1; i++){
-			if(bobBall.intersects(walls.get(i))){
+		for (Shape w: walls){
+			if(bobBall.getBox().intersects(w)){
+				System.out.println("Got here 0");
+				float maxX = bobBall.getBox().getMaxX();
+				float minX = bobBall.getBox().getMinX();
+				float maxY = bobBall.getBox().getMaxY();
+				float minY = bobBall.getBox().getMinY();
 				
-				boolean cond1 = bobBall.intersectsRight(walls.get(i));
-				boolean cond2 = bobBall.intersectsLeft(walls.get(i));
-				boolean cond3 = bobBall.intersectsBottom(walls.get(i));
-				boolean cond4 = bobBall.intersectsTop(walls.get(i));
-				float dampening = 0.45f;
-				if (cond1){
-					bobBall.setX(bobBall.getX()-1);
-					xBob = bobBall.getX();
+				float shapeX = w.getMaxX();
+				float shapex = w.getMinX();
+				float shapeY = w.getMaxY();
+				float shapey = w.getMinY();
+				
+				if(bobBall.getVelocityX() < 0){
+					System.out.println("Got here -v X");
+					if (bobBall.getVelocityY() > 0){
+						System.out.println("Got here -v Y");
+						if (minX <= shapeX && (maxY <= shapeY && minY >= shapey)){
+							bobBall.setX(bobBall.getX()+1);
+							xBob = bobBall.getX();
+							System.out.println("Got here 2");
+							bobBall.setVelocity(-bobBall.getVelocityX(), bobBall.getVelocityY());
+							lastCol = 2;
+						}
+						if (maxY >= shapey && (minX >= shapex && maxX <= shapeX)){
+							bobBall.setY(bobBall.getY()+1);
+							yBob = bobBall.getY();
+							System.out.println("Got here 4");
+							bobBall.setVelocity(bobBall.getVelocityX(), -bobBall.getVelocityY()*0.5f);
+							lastCol = 4;
+						}
+					}
+					else{
+						if (minX <= shapeX && (maxY <= shapeY && minY >= shapey)){
+							bobBall.setX(bobBall.getX()+1);
+							xBob = bobBall.getX();
+							System.out.println("Got here 2");
+							bobBall.setVelocity(-bobBall.getVelocityX(), bobBall.getVelocityY());
+							lastCol = 2;
+						}
+					}
+				}
+				else{
+					System.out.println("Got here +v X");
+					if (bobBall.getVelocityY() < 0){
+						System.out.println("Got here +v Y");
+						if (maxX >= shapex && (maxY <= shapeY && minY >= shapey)){
+							bobBall.setX(bobBall.getX()-10);
+							xBob = bobBall.getX(time);
+							System.out.println("Got here 1");
+							bobBall.setVelocity(-bobBall.getVelocityX(), bobBall.getVelocityY());
+							lastCol = 1;
+						}
+						if(minY <= shapeY && (minX >= shapex && maxX <= shapeX)){
+							bobBall.setY(bobBall.getY()-1);
+							yBob = bobBall.getY();
+							System.out.println("Got here 3");
+							bobBall.setVelocity(bobBall.getVelocityX(), -bobBall.getVelocityY());
+							lastCol = 3;
+							collisions += 1;
+						}
+					}
+					else{
+						if (maxX >= shapex && (maxY <= shapeY && minY >= shapey)){
+							bobBall.setX(bobBall.getX()-10);
+							xBob = bobBall.getX(time);
+							System.out.println("Got here 1");
+							bobBall.setVelocity(-bobBall.getVelocityX(), bobBall.getVelocityY());
+							lastCol = 1;
+						}
+					}
+				}
+				
+//				if(maxX >= shapex && (maxY <= shapeY && minY >= shapey)){
+//					bobBall.setX(bobBall.getX()-10);
+//					xBob = bobBall.getX(time);
 //					System.out.println("Got here 1");
-					bobBall.setVelocity(-bobBall.getVelocityX()*dampening, bobBall.getVelocityY());
-					lastCol = 1;
-					break;
-				}
-				if (cond2){
-					bobBall.setX(bobBall.getX()+1);
-					xBob = bobBall.getX();
+//					bobBall.setVelocity(-bobBall.getVelocityX(), bobBall.getVelocityY());
+//					lastCol = 1;
+//				}
+//				if (minX <= shapeX && (maxY <= shapeY && minY >= shapey)){
+//					bobBall.setX(bobBall.getX()+1);
+//					xBob = bobBall.getX();
 //					System.out.println("Got here 2");
-					bobBall.setVelocity(-bobBall.getVelocityX()*dampening, bobBall.getVelocityY());
-					lastCol = 2;
-					break;
-				}
-				
-				if(cond3){
-					bobBall.setY(bobBall.getY()-1);
-					yBob = bobBall.getY();
-					System.out.println("Got here 3");
-					bobBall.setVelocity(bobBall.getVelocityX(), -bobBall.getVelocityY()*dampening);
-					lastCol = 3;
-					collisions += 1;
-					break;
-				}
-				if(cond4){
-					bobBall.setY(bobBall.getY()+1);
-					yBob = bobBall.getY();
-					System.out.println("Got here 4");
-					bobBall.setVelocity(bobBall.getVelocityX(), -bobBall.getVelocityY()*0.5f);
-					lastCol = 4;
-					break;
-				}
-			
-			
+//					bobBall.setVelocity(-bobBall.getVelocityX(), bobBall.getVelocityY());
+//					lastCol = 2;
+//				
+//				}
+//				
+//				if(cond3){
+//					bobBall.setY(bobBall.getY()-1);
+//					yBob = bobBall.getY();
+//					System.out.println("Got here 3");
+//					bobBall.setVelocity(bobBall.getVelocityX(), -bobBall.getVelocityY()*dampening);
+//					lastCol = 3;
+//					collisions += 1;
+//			
+//				}
+//				if(cond4){
+//					bobBall.setY(bobBall.getY()+1);
+//					yBob = bobBall.getY();
+//					System.out.println("Got here 4");
+//					bobBall.setVelocity(bobBall.getVelocityX(), -bobBall.getVelocityY()*0.5f);
+//					lastCol = 4;
+//				
+//				}
+//			
+//			
 			}
 		}
 	}
